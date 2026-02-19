dfc2b5eda630a56a6c1d8037c4ef33f33b6e0a6d
tbag86 <tbag86@users.noreply.github.com>
2026-02-19 21:00:38 +0000

    Fix: switch to Vercel adapter for Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://cashmeup.co.uk',
@@ -10,5 +10,5 @@ export default defineConfig({
     sitemap(),
   ],
   output: 'static',
-  adapter: node({ mode: 'standalone' }),
+  adapter: vercel(),
 });
